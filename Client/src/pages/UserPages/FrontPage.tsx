6bb1f7774cf2500da45c81cc5d88b1071fe0d643
Joakosss <jo.perezu@duocuc.cl>
2025-05-18 14:00:30 -0400

    Probando cositas

--- Client/src/pages/UserPages/FrontPage.tsx
+++ Client/src/pages/UserPages/FrontPage.tsx
@@ -1,5 +1,4 @@
 import CategoryPreviews from "../../components/CategoryPreviews";
-import PromoSection from "../../components/PromoSection";
 import TrendingProducts from "../../components/TrendingProducts";
 
 type Props = {};
@@ -15,3 +14,30 @@ function FrontPage({}: Props) {
 }
 
 export default FrontPage;
+
+function PromoSection() {
+  return (
+    <>
+        {/* <!-- Callout --> */}
+        <section
+          aria-labelledby="sale-heading"
+          className="relative mx-auto flex max-w-7xl flex-col items-center px-4 p-32 text-center sm:px-6 lg:px-8"
+        >
+          <div className="mx-auto max-w-2xl lg:max-w-none">
+            <h2
+              id="sale-heading"
+              className="text-4xl font-bold tracking-tight text-primary sm:text-5xl lg:text-6xl"
+            >
+              ¡Lleva Más, Paga Menos!
+            </h2>
+            <p className="mx-auto mt-4 max-w-xl text-xl text-primary/70">
+              <strong>
+                Después del 4to producto, obtén un 25% de descuento en todo lo
+                adicional.
+              </strong>
+            </p>
+          </div>
+        </section>
+    </>
+  );
+}

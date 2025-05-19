294b80f8f0df816c2dd9e7fef46d314f0b64441e
Joakosss <jo.perezu@duocuc.cl>
2025-05-19 15:34:46 -0400

    colores c:

--- Client/src/components/CategoryPreviews.tsx
+++ Client/src/components/CategoryPreviews.tsx
@@ -34,7 +34,7 @@ export default function CategoryPreviews() {
     <div className="bg-gray-100">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-          <h2 className="text-2xl font-bold text-gray-900">Colecciones</h2>
+          <h2 className="text-2xl font-bold text-primary">Algunas Categorías</h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             {callouts.map((callout) => (
@@ -45,13 +45,13 @@ export default function CategoryPreviews() {
                   className="w-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-2/1 lg:aspect-square"
                   loading="lazy"
                 />
-                <h3 className="mt-6 text-sm text-gray-500">
+                <h3 className="mt-6 text-sm text-primary/70">
                   <a href={callout.href}>
                     <span className="absolute inset-0" />
                     {callout.name}
                   </a>
                 </h3>
-                <p className="text-base font-semibold text-gray-900">
+                <p className="text-base font-semibold text-primary/90">
                   {callout.description}
                 </p>
               </div>
--- Client/src/components/TrendingProducts.tsx
+++ Client/src/components/TrendingProducts.tsx
@@ -29,13 +29,13 @@ export default function Example() {
 
   const handleNavigate = (product: ProductType) => {
     const slugName = generateSlug(product.nombre);
-    navigate(`/producto/${product.id}/${slugName}`);
+    navigate(`/producto/${product.id_producto}/${slugName}`);
   };
 
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        <h2 className="text-2xl font-bold tracking-tight text-primary">
           Productos Populares
         </h2>
 
@@ -50,7 +50,7 @@ export default function Example() {
             !isError &&
             products &&
             products.map((product) => (
-              <div key={product.id} className="group relative">
+              <div key={product.id_producto} className="group relative">
                 <img
                   alt={product.nombre}
                   src={product.link_foto}

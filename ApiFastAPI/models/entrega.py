bb131dd4a8e11129c61ba5b14b7d44d2da10b5ed
Zireaeell <caro.pacheco@duocuc.cl>
2025-05-22 20:12:06 -0400

    dirección entrega opcional

--- ApiFastAPI/models/entrega.py
+++ ApiFastAPI/models/entrega.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 class Entrega(SQLModel, table=True):
     id_entrega: str = Field(default_factory=lambda: str(uuid.uuid4()), max_length=50, primary_key=True)
     fecha_entrega: datetime.date = Field(nullable=False)
-    direccion_entrega: str = Field(max_length=200, nullable=False)
+    direccion_entrega: str = Field(max_length=200, nullable=True)
     id_sucursal: str = Field(max_length=50, foreign_key="sucursal.id_sucursal", nullable=True)
     id_estado_entrega: str = Field(max_length=50, foreign_key="estado_entrega.id_estado_entrega", nullable=False)
     id_tipo_entrega: str = Field(max_length=50, foreign_key="tipo_entrega.id_tipo_entrega", nullable=False)

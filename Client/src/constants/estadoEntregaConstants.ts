0f830c4ae31657efea65187c4343bb796c52f4d4
PAAWSA02LC1007 <PAAWSA02LC1007@PAAWSA02LC1007.duoc.acad>
2025-05-29 15:55:44 -0400

    modificamos algo

--- Client/src/constants/estadoEntregaConstants.ts
+++ Client/src/constants/estadoEntregaConstants.ts
@@ -3,4 +3,5 @@ export const estadoEntregaConstants = [
   { id: "1", descripcion: "Pendiente" },
   { id: "2", descripcion: "Cancelado" },
   { id: "3", descripcion: "Entregado" },
+  { id: "4", descripcion: "Completado" },
 ];

9a2c613e2ab17af32213f69bb8b32cbf1c245487
Joakosss <jo.perezu@duocuc.cl>
2025-05-29 21:33:22 -0400

    informes nuevos

---
+++ Client/src/hooks/NewQuerys/informesQuerys/useComprasxCliente.ts
@@ -0,0 +1,69 @@
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import useAuthStore from "../../../store/useAuthStore";
+import { useNavigate } from "react-router-dom";
+import { utils, writeFile } from "xlsx";
+
+const axiosQuery = async (accessToken: string) => {
+  return await axios.get("http://localhost:8000/api/informes/compras_por_cliente", {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+};
+
+function useComprasxCliente() {
+  const { tokens, setAuth, logout } = useAuthStore();
+  const navigate = useNavigate();
+  return useMutation({
+    mutationKey: ["ventas_mes"],
+    mutationFn: async () => {
+      if (!tokens || !tokens.access_token || !tokens.refresh_token) {
+        logout();
+        navigate("/");
+        throw new Error(
+          "Sesión expirada. Por favor, inicie sesión nuevamente."
+        );
+      }
+      try {
+        const response = await axiosQuery(tokens.access_token);
+        console.log(response.data);
+        return response.data;
+      } catch (error: any) {
+        const errorAxios = error as AxiosError;
+        if (errorAxios.response?.status === 401) {
+          /* Aqui se hace el refresh del token */
+          try {
+            const response = await axios.post(
+              "http://127.0.0.1:8000/api/auth/refresh",
+              { refresh_token: tokens.refresh_token }
+            );
+            setAuth({
+              access_token: response.data.access_token,
+              refresh_token: tokens.refresh_token,
+              autorization: tokens.autorization,
+              id_usuario: tokens.id_usuario,
+            });
+            /* Se realiza la consulta nuevamente con el nuevo auth */
+            const retryResponse = await axiosQuery(response.data.access_token);
+            return retryResponse.data;
+          } catch (error: any) {
+            navigate("/");
+            logout();
+            throw new Error(error.response?.data.detail || "Sesión expirada");
+          }
+        }
+        throw new Error(
+          error.response?.data.detail || "La solicitud de stock bajo falló"
+        );
+      }
+    },
+    onSuccess: (data) => {
+      const wb = utils.book_new();
+      const ws = utils.json_to_sheet(data);
+      // Añadir la hoja al libro
+      utils.book_append_sheet(wb, ws, "Compras cliente");
+      writeFile(wb, "reporte_compras_cliente.xlsx"); //descarga el excel con los datos 
+    },
+  });
+}
+
+export default useComprasxCliente;
--- Client/src/pages/DashBoard/WorkSpaces/InformesTable.tsx
+++ Client/src/pages/DashBoard/WorkSpaces/InformesTable.tsx
@@ -1,8 +1,11 @@
 import LoadingOverlay from "../../../components/LoadingOverlay";
+import useComprasxCliente from "../../../hooks/NewQuerys/informesQuerys/useComprasxCliente";
 import useEntregasSucursal from "../../../hooks/NewQuerys/informesQuerys/useEntregasSucursal";
 import useProductosVendidos from "../../../hooks/NewQuerys/informesQuerys/useProductosVendidos";
 import useStockBajo from "../../../hooks/NewQuerys/informesQuerys/useStockBajo";
 import useStockProductos from "../../../hooks/NewQuerys/informesQuerys/useStockProductos";
+import useVentasxAnnio from "../../../hooks/NewQuerys/informesQuerys/useVentasxAnnio";
+import useVentasxMes from "../../../hooks/NewQuerys/informesQuerys/useVentasxMes";
 
 function InformesTable() {
   const { mutate: mutEntregasSucu, isPending: loadEntregasSucu } =
@@ -12,6 +15,12 @@ function InformesTable() {
   const { mutate: mutStockBajo, isPending: loadStockBajo } = useStockBajo();
   const { mutate: mutStockProd, isPending: loadStockProd } =
     useStockProductos();
+  const { mutate: mutComprasxCliente, isPending: loadComprasxCliente } =
+    useComprasxCliente();
+  const { mutate: mutVentasxAnnio, isPending: loadVentasxAnnio } =
+    useVentasxAnnio();
+  const { mutate: mutuseVentasxmes, isPending: loaduseVentasxmes } =
+    useVentasxMes();
 
   return (
     <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
@@ -39,9 +48,31 @@ function InformesTable() {
       >
         Productos vendidos
       </div>
+      <div
+        className="cursor-pointer border-primary/80 bg-primary/10 p-19 rounded text-center text-3xl font-bold text-primary hover:bg-primary/30"
+        onClick={() => mutVentasxAnnio()}
+      >
+        Ventas por Año
+      </div>
+      <div
+        className="cursor-pointer border-primary/80 bg-primary/10 p-19 rounded text-center text-3xl font-bold text-primary hover:bg-primary/30"
+        onClick={() => mutuseVentasxmes()}
+      >
+        Ventas por año
+      </div>
+      <div
+        className="cursor-pointer border-primary/80 bg-primary/10 p-19 rounded text-center text-3xl font-bold text-primary hover:bg-primary/30"
+        onClick={() => mutComprasxCliente()}
+      >
+        Compras por Cliente
+      </div>
+
       {loadStockBajo ||
         loadEntregasSucu ||
         loadProductosVend ||
+        loadComprasxCliente ||
+        loadVentasxAnnio ||
+        loaduseVentasxmes ||
         (loadStockProd && <LoadingOverlay />)}
     </div>
   );
